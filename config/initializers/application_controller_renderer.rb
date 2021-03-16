a0a7df855d7f30e5a7749d4998b612723015ca7e
okunoyuto0000 <okunoyuto0000@gmail.com>
2021-03-16 11:09:41 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a6c70d9daab5dda5caa6e49c6faa30e037d3e6ed5dda7ffa6e6be7388b60b578f7654554ef3e11af61b7b5b44e81f47ee7ffb71b7feb848c35cdfd3b74851e3c'
